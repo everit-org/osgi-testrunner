8a21739e0cc7f7bbdcbdc05b2cdde1ed0548b4c3
Balázs Zsoldos <balazs.zsoldos@everit.biz>
2017-01-06 14:33:57 +0100

    Refactorings for 5.0.0

--- src/main/java/org/everit/osgi/dev/testrunner/util/testclasscapability/TestClassCapabilityUtil.java
+++ src/main/java/org/everit/osgi/dev/testrunner/util/testclasscapability/TestClassCapabilityUtil.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.everit.osgi.dev.testrunner.testclasscapability.util;
+package org.everit.osgi.dev.testrunner.util.testclasscapability;
 
 import java.util.ArrayList;
 import java.util.Collection;
